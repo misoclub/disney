f65a35bec3f37823b22fc85261e6c697734ed699
Takafumi Naito <takafumi.naito@dena.com>
2020-07-05 15:10:31 +0900

    Updates

---
+++ precache-manifest.8d9b47b314115b84a65f2d8fb8557643.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c459bb83b56d6c9a557bee17e13c7bb7",
+    "url": "/disney/index.html"
+  },
+  {
+    "revision": "08a5fb96747bb8b8838d",
+    "url": "/disney/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "50c4915ba74cbc25f43a",
+    "url": "/disney/static/js/2.8233a274.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/disney/static/js/2.8233a274.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08a5fb96747bb8b8838d",
+    "url": "/disney/static/js/main.3291d225.chunk.js"
+  },
+  {
+    "revision": "a5139cc1ad8784296bf7",
+    "url": "/disney/static/js/runtime-main.7b4aa3b4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/disney/static/media/logo.5d5d9eef.svg"
+  }
+]);
